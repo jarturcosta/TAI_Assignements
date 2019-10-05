f572cf1859412ec2cfd29e9d38ebbcd64fd1937c
João Costa <jarturcosta@ua.pt>
2019-10-05 20:42:14 +0100

    Reads file

--- fcm.cpp
+++ fcm.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
+
+
+string readFile(string filename) {
+    string line;
+    ifstream myfile (filename);
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    return line;
+}
+
+
 int main() 
 {
-    cout << "Hello, World!";
+    cout << readFile("genes.txt");
+
     return 0;
 }
